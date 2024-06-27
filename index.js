684602a0446d304e6df366d595feb75aa8c472b0
NashiDetki <93508687+NashiDetki@users.noreply.github.com>
2024-06-27 11:26:34 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+const buttonPlay = document.querySelector(".button_play");
+console.log(buttonPlay);
+const audio = document.querySelector('.audio')
+console.log(audio);
+
+buttonPlay.addEventListener('click',()=>{
+    if (audio.paused){
+         audio.src = './track.mp3';
+        audio.play();
+        audio.valume = 0.2;
+        console.log('pley');
+    } else {
+        audio.pause();
+        console.log('pause')
+    }
+})
